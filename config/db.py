303b0684b1a4493dcf53d0536cd09b0d43609dd4
Darkcheftar <darkcheftar.connect@gmail.com>
2023-04-24 21:43:25 +0530

    added basic db config

---
+++ config/db.py
@@ -0,0 +1,9 @@
+from pymongo import MongoClient
+
+database = MongoClient('mongodb://localhost:27017')
+users_collection = database.access_control.users
+organisations_collection = database.access_control.organisations
+if 'user_name_index' not in users_collection.index_information():
+    users_collection.create_index('name',name="user_name_index")
+if 'org_name_index' not in organisations_collection.index_information():
+    organisations_collection.create_index('name',name='org_name_index',unique=True)
