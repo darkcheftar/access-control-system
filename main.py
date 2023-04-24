85d3882bea3683fe81029388cc6068f130debe13
Darkcheftar <darkcheftar.connect@gmail.com>
2023-04-24 22:10:52 +0530

    /organisation router included in main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.user import user
+from routes.organisation import organisation
 
 app = FastAPI()
 
 app.include_router(user, prefix='/user')
+app.include_router(organisation, prefix='/organisation')
 @app.get('/')
 def read_root():
     return "go to /user"
